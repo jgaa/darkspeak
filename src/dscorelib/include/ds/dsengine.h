ef13624a571bfad5d042cec15e29d992da87a201
Jarle Aase <jarle@jgaa.com>
2018-02-28 20:56:27 +0200

    Started to defibne the interface of the core IM engine.

--- src/dscorelib/include/ds/dsengine.h
+++ src/dscorelib/include/ds/dsengine.h
@@ -1,12 +1,57 @@
 #ifndef DSENGINE_H
 #define DSENGINE_H
 
+#include <QObject>
 
-class DsEngine
+namespace ds {
+namespace core {
+
+class Identity;
+class IdentityReq;
+class IdentityError;
+class TransportHandle;
+class TransportHandleReq;
+class TransportHandleError;
+
+/*! The core engine of DarkSpeak.
+ *
+ * It owns the database, and it acts like a layer
+ * between the QT Models above and the actual protocol
+ * below.
+ *
+ * There is one instance of this object in a DarkSpeak application.
+ */
+class DsEngine : public QObject
 {
+    Q_OBJECT
+    DsEngine();
 
 public:
-    DsEngine();
+    enum State {
+        INITIALIZING,
+        RUNNING,
+        CLOSING,
+        TERMINATED
+    };
+
+    DsEngine& instance();
+    State getState() const;
+
+public slots:
+    void createIdentity(const IdentityReq&);
+    void createTransportHandle(const TransportHandleReq&);
+    void close();
+
+signals:
+    void identityCreated(const Identity&);
+    void identityError(const IdentityError&);
+    void transportHandleReady(const TransportHandle&);
+    void transportHandleError(const TransportHandleError&);
+    void closing();
+    void changedState(const State);
+
 };
+
+}} // namepsaces
 
 #endif // DSENGINE_H
